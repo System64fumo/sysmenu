aac8040b438e8a59f5eae54dd3aac072bca75836
system64fumo <system64fumo@protonmail.com>
2024-06-21 02:19:11 +0300

    Add dock item
    Basic dock item implementation

--- src/dock.hpp
+++ src/dock.hpp
@@ -1,9 +1,15 @@
 #pragma once
 #include <gtkmm/box.h>
+#include <gtkmm/button.h>
+#include <giomm/desktopappinfo.h>
+
+class dock_item : public Gtk::Button {
+	public:
+		dock_item(Glib::RefPtr<Gio::AppInfo> app);
+};
 
 class dock : public Gtk::Box {
 	public:
 		dock();
-
-	private:
+		void load_items(std::vector<std::shared_ptr<Gio::AppInfo>> items);
 };
