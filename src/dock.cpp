b2df6edf1431247fb4b455ace8aef632edc9b3a8
system64fumo <system64fumo@protonmail.com>
2024-06-22 21:46:53 +0300

    Add ability to launch dock items
    Self explanatory really.

--- src/dock.cpp
+++ src/dock.cpp
@@ -4,6 +4,9 @@
 dock::dock() {
 	get_style_context()->add_class("dock");
 	set_halign(Gtk::Align::CENTER);
+	property_orientation().set_value(Gtk::Orientation::VERTICAL);
+	set_selection_mode(Gtk::SelectionMode::NONE);
+	signal_child_activated().connect(sigc::mem_fun(*this, &dock::on_child_activated));
 }
 
 void dock::load_items(std::vector<std::shared_ptr<Gio::AppInfo>> items) {
@@ -20,17 +23,24 @@ void dock::load_items(std::vector<std::shared_ptr<Gio::AppInfo>> items) {
 			continue;
 
 		dock_existing_items = dock_existing_items + name;
-		dock_item item(app_info);
-		append(item);
+		auto item = Gtk::make_managed<dock_item>(app_info);
+		append(*item);
 	}
 }
 
+void dock::on_child_activated(Gtk::FlowBoxChild* child) {
+	dock_item *button = dynamic_cast<dock_item*>(child);
+	button->app_info->launch(std::vector<Glib::RefPtr<Gio::File>>());
+}
+
 dock_item::dock_item(Glib::RefPtr<Gio::AppInfo> app) {
+	app_info = app;
 	get_style_context()->add_class("dock_item");
 	set_size_request(64, 64);
 
-	// TODO: Add on_click event
-	append(image_icon);
+	set_child(image_icon);
 	image_icon.set(app->get_icon());
 	image_icon.set_pixel_size(64);
+
+	set_focus_on_click(false);
 }
