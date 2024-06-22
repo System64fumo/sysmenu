3595f5487942c0dd8ac4883870b4aea4bf49aba3
system64fumo <system64fumo@tuta.io>
2024-06-22 21:46:53 +0300

    Add ability to launch dock items
    Self explanatory really.

--- src/dock.hpp
+++ src/dock.hpp
@@ -1,21 +1,23 @@
 #pragma once
-#include <gtkmm/box.h>
+#include <gtkmm/flowbox.h>
 #include <gtkmm/image.h>
 #include <giomm/desktopappinfo.h>
 
-class dock_item : public Gtk::Box {
+class dock_item : public Gtk::FlowBoxChild {
 	public:
 		dock_item(Glib::RefPtr<Gio::AppInfo> app);
+		Glib::RefPtr<Gio::AppInfo> app_info;
 
 	private:
 		Gtk::Image image_icon;
 };
 
-class dock : public Gtk::Box {
+class dock : public Gtk::FlowBox {
 	public:
 		dock();
 		void load_items(std::vector<std::shared_ptr<Gio::AppInfo>> items);
 
 	private:
 		std::string dock_existing_items;
+		void on_child_activated(Gtk::FlowBoxChild* child);
 };
