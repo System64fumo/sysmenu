99b10f3971d04b8d2bb35e5635f4edffa10026ba
system64fumo <system64fumo@protonmail.com>
2024-07-10 10:55:36 +0300

    Bugfix
    "Another One" - DJ Khaled
    
    Sorta forgot to inline the handle signal function in main.hpp, Whoops!
    Dock now works as intended with signals.

--- src/main.hpp
+++ src/main.hpp
@@ -8,4 +8,4 @@ inline sysmenu* win;
 typedef sysmenu* (*sysmenu_create_func)(const config &cfg);
 typedef void (*sysmenu_handle_signal_func)(sysmenu*, int);
 inline sysmenu_create_func sysmenu_create_ptr;
-sysmenu_handle_signal_func sysmenu_handle_signal_ptr;
+inline sysmenu_handle_signal_func sysmenu_handle_signal_ptr;
